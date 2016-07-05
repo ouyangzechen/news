14f9f77d44a2f15a64c08212c8834f08e4c66a9c
ouyangzechen <657672853@qq.com>
2016-07-05 22:28:59 +0800

    update

--- news/getNewsData.php
+++ news/getNewsData.php
@@ -1,36 +1,6 @@
 <?php
-$url ="http://weixin.sogou.com/weixin?type=1&query=%E6%B5%B7%E6%99%AE%E6%B4%9B%E6%96%AF";
-$data=file_get_contents($url);
-
-preg_match("/gotourl\('(.*)',event,this/",$data,$str_array);
-if(!count($str_array)>0){
-    exit;
-}
-$url =$str_array[1];
-
-//echo $url;
-
-$url = str_replace('&amp;', '&', $url);
-//$url = 'http://mp.weixin.qq.com/profile?src=3&timestamp=1467682135&ver=1&signature=klwAfc5RUSxsmHSqZyam9nrClhy6muENJeYZzjln*b4pCsXTSzAobOIo8R8L-3z98vljOP3JTCZSd1G4TwQ7Rw==';
-
-$content = file_get_contents($url);
-
-
-$startPos = strpos($content, 'var msgList = ');
-if($startPos > 10)
-{
-    $endPos = strpos($content, "}}]}';");
-
-    $json = trim(substr($content, $startPos+15, $endPos-($startPos+11)));
-
-    var_dump( json_decode( htmlspecialchars_decode($json) ) );
-}exit;
 require_once './newsData.php';
 header("Content-type: text/html; charset=utf-8");
-
-
-
-
     if(!isset($createTime)||time()-$createTime>60*60*1) {
 
         $url ="http://weixin.sogou.com/weixin?type=1&query=%E6%B5%B7%E6%99%AE%E6%B4%9B%E6%96%AF";
@@ -49,7 +19,7 @@ header("Content-type: text/html; charset=utf-8");
 
         $content = file_get_contents($url);
 
-
+        $json='';
         $startPos = strpos($content, 'var msgList = ');
         if($startPos > 10)
         {
@@ -57,21 +27,20 @@ header("Content-type: text/html; charset=utf-8");
 
             $json = trim(substr($content, $startPos+15, $endPos-($startPos+11)));
 
-            var_dump( json_decode( htmlspecialchars_decode($json) ) );
-        }
-//        preg_match_all('/{title:(.*?)digest:(.*?)content:,fileid:(.*?),content_url:(.*?),source_url:(.*?),cover:(.*?),subtype/',$data,$str_array);
-//        $arrayDict =array(1=>"title",2=>"digest",3=>"fileid",4=>"content_url",5=>"source_url",6=>"cover");
-        $array = array();
+            $json = json_decode( htmlspecialchars_decode($json) )->list;
 
-        for($i=1;$i<count($str_array);$i++){
-            for($j=0;$j<count($str_array[$i]);$j++){
-                $array[$j][$arrayDict[$i]]=$str_array[$i][$j];
+            $array=array();
+            for($i=0;$i<count($json);$i++){
+                $array[$i]['title'] = $json[$i]->app_msg_ext_info->title;
+                $array[$i]['digest'] = $json[$i]->app_msg_ext_info->digest;
+                $array[$i]['content_url'] =str_replace('\/','/','http://mp.weixin.qq.com'.$json[$i]->app_msg_ext_info->content_url);
+                $array[$i]['cover'] = str_replace('\/','/',$json[$i]->app_msg_ext_info->cover);
+
             }
+            $json =json_encode($array);
         }
-//        var_dump(json_encode($array));exit;
-        $json=json_encode($array);
-        $json=str_replace("wx_fmt=","wx_fmt.",$json);
-        $json=str_replace('\\','',$json);
+//      preg_match_all('/{title:(.*?)digest:(.*?)content:,fileid:(.*?),content_url:(.*?),source_url:(.*?),cover:(.*?),subtype/',$data,$str_array);
+
         write($json);
         echo $json;
         exit;
